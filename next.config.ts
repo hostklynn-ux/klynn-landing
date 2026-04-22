210636939fe5690153bbdd9a97ddf15e764003a6
Gerson <academicogersonfigeliskt@gmail.com>
2026-04-21 20:24:22 -0400

    fix: enable static export for cloudflare

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'standalone',
-
-  // API rewrite for development
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path((?!auth).*)',
-        destination: `${process.env.NEXT_PUBLIC_API_URL}/:path*` || 'http://localhost:4000/api/:path*',
-      },
-    ];
-  },
+  output: "export",
 };
 
 export default nextConfig;
